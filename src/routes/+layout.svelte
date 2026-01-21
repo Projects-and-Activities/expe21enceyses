4c0e7d017d4816eb21a7014e436f9dd8d03d3c1e
Arvin Ferrer <60701881+arvin-ferrer@users.noreply.github.com>
2026-01-21 16:17:03 +0800

    feat: add liquid glass appbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,17 @@
 <script lang="ts">
 	import './layout.css';
 	import favicon from '$lib/assets/favicon.svg';
+	import Navbar from '$lib/components/Navbar.svelte';
 
 	let { children } = $props();
 </script>
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
-{@render children()}
+
+<div class="min-h-screen bg-white font-['Lexend']">
+	<Navbar />
+	<main class="pt-[140px]">
+		{@render children()}
+	</main>
+</div>
+
