37d59326eb352275cb68dc53bf0524e1c36dd6df
Iorie Chua <ioriecodez@gmail.com>
2026-01-27 18:56:15 +0800

    chore: fix import order of eslint and vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
+import { resolve } from 'path';
+
 import { enhancedImages } from '@sveltejs/enhanced-img';
 import { sveltekit } from '@sveltejs/kit/vite';
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite';
-import { resolve } from 'path';
 
 export default defineConfig({
 	plugins: [tailwindcss(), enhancedImages(), sveltekit()],
