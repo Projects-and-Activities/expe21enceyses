7ca3e54f85219d62374e9d53e55daff4c54da82d
rfquejada <rfquejada@up.edu.ph>
2026-01-30 23:20:21 +0800

    refactor: fix file convention

---
+++ src/lib/cloudinary/config.ts
@@ -0,0 +1,5 @@
+import { PUBLIC_CLOUDINARY_CLOUD_NAME } from "$env/static/public";
+
+export const CLOUDINARY_BASE_URL = `https://res.cloudinary.com/${PUBLIC_CLOUDINARY_CLOUD_NAME}/image/upload/`;
+
+// TODO: Add cloudinary config if needed in the future
--- src/lib/cloudinary/constants.ts
+++ src/lib/cloudinary/constants.ts
@@ -1,6 +1,4 @@
-import { PUBLIC_CLOUDINARY_CLOUD_NAME } from "$env/static/public";
-
-export const CLOUDINARY_BASE_URL = `https://res.cloudinary.com/${PUBLIC_CLOUDINARY_CLOUD_NAME}/image/upload/`;
+import { CLOUDINARY_BASE_URL } from "./config";
 
 // Cloudinary image constants
 export const CLOUDINARY_IMAGES = {
