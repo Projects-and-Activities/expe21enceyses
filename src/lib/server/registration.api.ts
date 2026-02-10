1f80709e863daa7bccc1124e58ea4636f610e218
rfquejada <rfquejada@up.edu.ph>
2026-02-10 18:40:09 +0800

    feat: add toBase64

--- src/lib/server/registration.api.ts
+++ src/lib/server/registration.api.ts
@@ -1,8 +1,22 @@
 import { 
   GOOGLE_APPS_SCRIPT_URL_COMPANY_TALKS,
-  GOOGLE_APPS_SCRIPT_URL_PFJF
+  GOOGLE_APPS_SCRIPT_URL_PFJF,
+  GOOGLE_APPS_SCRIPT_URL_HACKFEST
 } from '$env/static/private';
 import { EVENT_IDS, type RegistrationData } from '../types/registration.types';
+
+async function toBase64(file: File): Promise<string> {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.onload = () => {
+      const dataUrl = reader.result as string;
+      const base64 = dataUrl.split(',')[1];
+      resolve(base64);
+    };
+    reader.onerror = () => reject(reader.error);
+    reader.readAsDataURL(file);
+  });
+}
 
 export async function submitRegistration(eventId: string, data: RegistrationData) {
   let script
